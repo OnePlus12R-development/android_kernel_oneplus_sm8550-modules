1bd8e24cdb1e8971b8938533d3a17954e9b81b3d
Vinod Kumar Pirla <quic_vpirla@quicinc.com>
2024-10-06 22:19:45 -0700

    [BACKPORT] qcacld-3.0: Vendor interface to handle zero power scan req
    Introduce APIs and register vendor interface for handling
    zero power scan requests from userspace.
    
    Change-Id: Id11f14df4ee768fbbc2372a660a298114332f35b
    CRs-Fixed: 3956251

--- qcom/opensource/wlan/qcacld-3.0/core/hdd/src/wlan_hdd_cfg80211.h
+++ qcom/opensource/wlan/qcacld-3.0/core/hdd/src/wlan_hdd_cfg80211.h
@@ -277,6 +277,7 @@ typedef enum {
 #define WIFI_FEATURE_SET_LATENCY_MODE   0x40000000 /* Set latency mode */
 /* Support changing MAC address without iface reset(down and up) */
 #define WIFI_FEATURE_DYNAMIC_SET_MAC    0x10000000
+#define WIFI_FEATURE_CACHED_SCAN_RESULTS 0x2000000000
 
 /* Support Tx Power Limit setting */
 #define WIFI_FEATURE_SET_TX_POWER_LIMIT 0x4000000
